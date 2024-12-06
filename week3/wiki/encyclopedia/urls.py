0eba25e6b318dda922c1517c6c6f473d42eb2b4a
Rosa <kosolrosa@gmail.com>
2024-12-06 21:09:35 +1100

    Created custom 404 error page. Defined view to display markdown entries as HTML pages.

--- week3/wiki/encyclopedia/urls.py
+++ week3/wiki/encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry")
 ]
+
+print("encyclopedia URL patterns:", urlpatterns)
--- week3/wiki/encyclopedia/views.py
+++ week3/wiki/encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from . import util
+import markdown
+
+
 
 
 def index(request):
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# Function that renders the page for each entry
+def entry(request, title):
+    content = util.get_entry(title)
+    if content:
+        html_content = markdown.markdown(content)
+        return render(request, 'encyclopedia/entry.html', {
+            'title': title, 
+            'content': html_content
+        })
+    # If entry does not exist, render custom 404 error page
+    return render(request, 'encyclopedia/404.html', {
+        'title': title.capitalize()
+    })
