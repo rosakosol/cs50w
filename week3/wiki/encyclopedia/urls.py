03e8b3c0405e29577cd9d42f1370229a05df087c
Rosa <kosolrosa@gmail.com>
2024-12-11 13:19:48 +1100

    Implemented Edit view where users can edit existing entry content, submit form and view the updated entry.

--- week3/wiki/encyclopedia/urls.py
+++ week3/wiki/encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/", views.index, name="index"),
+    path("wiki/edit/<str:title>", views.edit, name="edit"),
     path("wiki/<str:title>", views.entry, name="entry")
-]
 
-print("encyclopedia URL patterns:", urlpatterns)
+]
--- week3/wiki/encyclopedia/views.py
+++ week3/wiki/encyclopedia/views.py
@@ -1,13 +1,68 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 import markdown
 
 
-
+class NewTaskForm(forms.Form):
+    edit_content = forms.CharField(
+        label='Content',
+        widget=forms.Textarea(attrs={
+            'rows': 10,
+            'cols': 80
+        })
+    )
+        
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+# Function that allows user to edit entry
+def edit(request, title):
+    content = util.get_entry(title)
+    if content:
+        # If POST request, then use the submitted form data
+        if request.method == "POST":
+            # Create a form variable and save user content
+            form = NewTaskForm(request.POST, initial={'edit_content': content})
+            # If form is valid, save new entry
+            if form.is_valid():
+                new_content = form.cleaned_data["edit_content"]
+                
+                # Remove extra new lines
+                new_content = "\n".join([line.lstrip() for line in new_content.splitlines() if line.strip()])
+                
+                util.save_entry(title, new_content)
+                
+                # Redirect to updated entry page
+                return HttpResponseRedirect(reverse("entry", args=[title]))
+                
+            # Else if form invalid, return the submitted form
+            else:
+                return render(request, 'encyclopedia/edit.html', {
+                    "entries": util.list_entries(),
+                    "title": title,
+                    "content": content,
+                    "form": form
+                })
+        # Render empty form        
+        form = NewTaskForm(initial={'edit_content': content})
+        
+        return render(request, "encyclopedia/edit.html", {
+            "entries": util.list_entries(),
+            "title": title,
+            "content": content,
+            "form": form
+        })
+            
+        
+    # If entry does not exist, render custom 404 error page
+    return render(request, 'encyclopedia/404.html', {
+        'title': title.capitalize()
     })
 
 # Function that renders the page for each entry
@@ -16,8 +71,9 @@ def entry(request, title):
     if content:
         html_content = markdown.markdown(content)
         return render(request, 'encyclopedia/entry.html', {
-            'title': title, 
-            'content': html_content
+            "entries": util.list_entries(),
+            "content": html_content,
+            "title": title
         })
     # If entry does not exist, render custom 404 error page
     return render(request, 'encyclopedia/404.html', {
