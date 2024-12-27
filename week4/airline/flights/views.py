462cfd6ff870d47c10bdf29af6d9fcf89208feb0
Rosa Kosol <kosolrosa@gmail.com>
2024-12-27 20:11:49 +1100

    Set up basic booking system for non-passengers

--- week4/airline/flights/views.py
+++ week4/airline/flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-from .models import Flight
+from .models import Flight, Passenger
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -9,13 +12,18 @@ def index(request):
     })
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(id=flight_id)
+    flight = Flight.objects.get(pk=flight_id)
 
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non-passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
-    
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
 
