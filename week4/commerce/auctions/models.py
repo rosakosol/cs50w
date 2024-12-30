6cc82cf65892bf8488f4b03dca452f0c512dee01
Rosa <kosolrosa@gmail.com>
2024-12-30 15:03:28 +1100

    Added distro code for commerce project

---
+++ week4/commerce/auctions/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Listings(models.Model):
+    pass
+
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
+
+
