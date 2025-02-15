474c517efbe3079385e9d67f8bdc0be3cc79a16a
Rosa <kosolrosa@gmail.com>
2025-02-15 18:49:14 +1100

    Created admin sites for ingredient, cuisine and recipe

--- week8/cookery/app/admin.py
+++ week8/cookery/app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, Cuisine, Recipe
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("name",)
+
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("name",)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "cuisine", "meal_type")
+    ordering = ("name",)
+    
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Cuisine, CuisineAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+    
--- week8/cookery/app/views.py
+++ week8/cookery/app/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Ingredient, Cuisine, Rating, MealType, Recipe
+from django.urls import reverse
+from .models import Ingredient, Cuisine, Rating, MealType, Recipe, CreateRecipeForm
+
 
 
 # Create your views here.
